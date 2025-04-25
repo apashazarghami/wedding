8e14b27f759d7b71d546ca43395b9d83e9719612
Amir Pasha <amirpashazarghami22@gmail.com>
2025-04-25 22:50:08 +0330

    init project

---
+++ src/components/background/MainBackground.jsx
@@ -0,0 +1,12 @@
+import lovelyImage from '../../images/IMG_1060.JPG'
+const MainBackground = () => {
+    return(
+        
+        <div
+          className={`w-full h-screen bg-scroll bg-cover bg-[url("../../images/IMG_1060.JPG")]`}
+        ></div>
+    )
+
+}
+
+export default MainBackground
